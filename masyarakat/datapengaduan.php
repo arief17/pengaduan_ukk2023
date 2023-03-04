9786ce2186164ebaec0885e9ea0ef4cbeaf3f544
ariffahrudin <ariffahrudin17@gmail.com>
2023-03-04 11:41:42 +0700

    menambahkan file test

--- masyarakat/datapengaduan.php
+++ masyarakat/datapengaduan.php
@@ -10,7 +10,7 @@ $tanggal = date("Y-m-d");
 
 <h4 class="mb-3 mt-3">Data Pengaduan Anda</h4>
 
-<a class="btn btn-primary btn-sm mb-3" data-toggle="modal" data-target="#exampleModal">+Tambah Pengaduan</a>
+<a class="btn btn-primary btn-sm mb-3" data-toggle="modal" data-target="#modaltambah">+Tambah Pengaduan</a>
 <table class="table">
   <thead>
     <tr>
@@ -30,7 +30,7 @@ $tanggal = date("Y-m-d");
     ?>
     <tr>
       <th scope="row"><?= $no++ ?></th>
-      <td><a href="#">Edit</a> | <a href="#">Hapus</a> | <a href="#">View</a></td>
+      <td><a href="#" data-toggle="modal" data-target="#modaledit">Edit</a> | <a href="#">Hapus</a> | <a href="#">View</a></td>
       <td><?= $ds['tgl_pengaduan']?></td>
       <td><?= $ds['isi_laporan']?></td>
       <td><?= $ds['status']?></td>
@@ -41,8 +41,8 @@ $tanggal = date("Y-m-d");
   </tbody>
 
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal Tambah-->
+<div class="modal fade" id="modaltambah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
@@ -75,6 +75,46 @@ $tanggal = date("Y-m-d");
     </div>
   </div>
 </div>
+
+
+<!-- Modal Edit-->
+<div class="modal fade" id="modaledit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <!-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> -->
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <h4 class="mt-2 mb-3">Form Edit Pengaduan Masyarakat</h4>
+
+      <form action="save_pengaduan.php" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+          <input type="hidden" name="tgl" class="form-control col-sm-5" id="exampleInputPassword1" value="<?= $tanggal?>">
+          <input type="hidden" name="nik" class="form-control col-sm-5" id="exampleInputPassword1" value="<?= $_SESSION['nik']?>">
+          <label for="exampleInputEmail1">Isi Pengaduan</label>
+          <textarea type="text"  name="isi" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Foto</label>
+          <input type="file" name="foto" class="form-control col-sm-5" id="exampleInputPassword1">
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Simpan Pengaduan</button>
+        </div>
+      </form>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+
+
 </table>
 
 
--- masyarakat/register_masyarakat.php
+++ masyarakat/register_masyarakat.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <title>Pengaduan Masyarakat - UKK 2023</title>
   </head>
   <body>
@@ -58,9 +57,9 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-
+     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+     <script src="../assets/js/jquery.slim.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
